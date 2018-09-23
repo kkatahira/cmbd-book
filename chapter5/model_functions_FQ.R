b60ab6294bdd782a3367bc8fa0cd328223152630
Kentaro Katahira <kkatahir@gmail.com>
2018-09-23 22:53:40 +0900

    update

--- chapter5/model_functions_FQ.R
+++ chapter5/model_functions_FQ.R
@@ -74,6 +74,7 @@ func_fqlearning <- function(param, data, prior = NULL)
     
     # choosing prob 1
     pA[t] <- 1/(1+exp(-beta * (Q[1,t]-Q[2,t])))
+    
     pA[t] <- max(min(pA[t], 0.9999), 0.0001)
     
     ll <- ll + (c[t]==1) * log(pA[t]) +  (c[t]==2) * log(1-pA[t])
@@ -149,80 +150,10 @@ func_dfqlearning <- function(param, data, prior = NULL)
   return(list(negll = -ll - lprior,Q = Q, pA = pA))
 }
 
-# 以下は5章では用いない (2章で出てきたモデル)
-
-# win-stay lose-shift
-func_wsls <- function(param, data, prior = NULL)
-{
-  
-  epsilon <- param[1] # error rate
-  c <- data$choice
-  r <- data$reward
-  T <- length(c)
-  
-  pA <- numeric(T) 
-  
-  # initialize log-likelihood
-  ll <- 0
-  
-  for (t in 1:T) {
-    
-    # choosing prob 1
-    if (t == 1) {
-      pA[t] <- 0.5
-    } else {
-      if (r[t-1]==1)
-        pA[t] <- (c[t-1]==1) * (1-epsilon) + (c[t-1]==2) * epsilon
-      else 
-        pA[t] <- (c[t-1]==1) * (epsilon) + (c[t-1]==2) * (1-epsilon)
-    }
-    
-    ll <- ll + (c[t]==1) * log(pA[t]) +  (c[t]==2) * log(1-pA[t])
-  }
-  
-  # log prior density 
-  if (is.null(prior)) {
-    lprior <- 0
-  } else {
-    lprior <- dbeta(epsilon, prior$epsilon_a, prior$epsilon_b, log = T)
-  }
-  
-  return(list(negll = -ll - lprior, pA = pA))
-}
-
-priorList <- list(
-  list(alpha_a = 2, alpha_b = 2, beta_shape = 2, beta_scale = 3), # q-learning
-  list(epsilon_a = 0.1, epsilon_b = 4)  # winstay-lose shift
-)
-
-# random choice
-func_random <- function(param, data, prior = NULL)
-{
-  
-  pA <- param[1] # choice prob
-  c <- data$choice
-  T <- length(c)
-  
-  # initialize log-likelihood
-  ll <- 0
-  
-  ll <- sum(c == 1) * log(pA) + sum(c == 2) * log(1-pA) 
-  
-  
-  # log prior density 
-  if (is.null(prior)) {
-    lprior <- 0
-  } else {
-    lprior <- dbeta(pA, prior$pA_a, prior$pA_b, log = T)
-  }
-  
-  return(list(negll = -ll - lprior, pA = pA))
-}
-
 # 事前分布のリスト
 
 priorList <- list(
-  list(alpha_a = 2, alpha_b = 2, beta_shape = 2, beta_scale = 3), # q-learning
+  list(alpha_a = 2, alpha_b = 2, beta_shape = 2, beta_scale = 3), # Q-learning
   list(alpha_a = 2, alpha_b = 2, beta_shape = 2, beta_scale = 3), # FQ-learning
   list(alpha_a = 2, alpha_b = 2, beta_shape = 2, beta_scale = 3) # DFQ-learning
 )

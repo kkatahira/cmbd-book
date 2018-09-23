b1b169e03a35f01fb255c72ad28a8c4b0f97ad7d
Kentaro Katahira <kkatahir@gmail.com>
2018-09-23 20:15:52 +0900

    update

--- chapter4/qlearning_simulation_Bayes_single.R
+++ chapter4/qlearning_simulation_Bayes_single.R
@@ -1,6 +1,7 @@
 # ----------------------------------------------------- #
-# Q学習モデルのシミュレーションにより一人分の選択データを生成し，
-# そのデータからベイズ推定，MAP推定をする。(比較のため最尤推定も行う)
+# Q学習モデルのシミュレーションにより一人分の
+# 選択データを生成し，そのデータからベイズ推定，MAP推定をする。
+# (比較のため最尤推定も行う)
 # ----------------------------------------------------- #
 
 # メモリ，グラフのクリア
@@ -12,10 +13,142 @@ library(tidyverse)
 library(gridExtra)
 library(rstan)  
 
+# 乱数のシードの設定
 set.seed(141)
 
-source("model_functions.R")
-source("parameter_fit_functions.R")
+# モデル関数の定義 ----------------------------------------------------------------
+
+# Q-learning
+func_qlearning <- function(param, data, prior = NULL)
+{
+  
+  alpha <- param[1]
+  beta <- param[2]
+  c <- data$choice
+  r <- data$reward
+  T <- length(c)
+  
+  pA <- numeric(T) 
+  
+  Q <- matrix(numeric(2*T), nrow=2, ncol=T)
+  
+  ll <- 0
+  
+  for (t in 1:T) {
+    
+    if (c[t] > 0) {
+      
+      pA[t] <- 1/(1+exp(-beta * (Q[1,t] - Q[2,t])))
+      
+      ll <- ll + (c[t]==1) * log(pA[t]) +  (c[t]==2) * log(1-pA[t])
+      
+      if (t < T) {
+        
+        Q[c[t],t+1] <- Q[c[t],t] + alpha * (r[t] - Q[c[t],t] ) 
+        
+        Q[3-c[t],t+1] <- Q[3-c[t],t]
+      }
+    } else {
+      if (t < T) 
+        Q[,t+1] <- Q[,t] 
+    }
+  }
+  
+  # 対数事後確率密度
+  if (is.null(prior)) {
+    lprior <- 0
+  } else {
+    lprior <- dbeta(alpha,prior$alpha_a, prior$alpha_b,log = T) + 
+      dgamma(beta,shape=prior$beta_shape, scale=prior$beta_scale,log = T) 
+  }
+  
+  return(list(negll = -ll - lprior,Q = Q, pA = pA))
+}
+
+# 事前分布のリスト
+priorList <- 
+  list(alpha_a = 2, alpha_b = 2, beta_shape = 2, beta_scale = 3)
+
+# パラメータ数
+nParam <- 2
+
+# パラメータ推定のための関数 -----------------------------------------------------------
+
+#　負の対数尤度のみを返すラッパー関数
+func_minimize <- function(modelfunc, param, data, prior)
+{
+  ret <- modelfunc(param, data, prior)
+  
+  # 負の対数尤度のみを返す
+  return(ret$negll)
+}
+
+# 最尤推定 
+paramfitML <- function(modelfunction, data, nParam)
+{
+  
+  fvalmin <- Inf
+  
+  for (idx in 1:10) {
+    
+    initparam <- runif(nParam, 0, 1.0)
+    
+    res <- optim(initparam, func_minimize,
+                 hessian = TRUE, modelfunc = modelfunction, 
+                 data=data, prior = NULL)
+    
+    if (res$value < fvalmin) {
+      paramest <- res$par
+      fvalmin <- res$value
+    }
+  }
+  aic <- 2*fvalmin + 2 * nParam
+  bic <- 2*fvalmin + nParam * log(T)
+  negll <- fvalmin
+  paramlist <- paramest
+  
+  print(sprintf("Estimated value: %.2f", paramest))
+  print(sprintf("log-likelihood: %.2f, AIC: %.2f, BIC: %.2f", 
+                negll, 
+                aic,
+                bic))
+  return(list(negll = negll, aic = aic, bic = bic, paramest = paramest))
+}
+
+
+# MAP推定 
+paramfitMAP <- function(modelfunction, data,  nParam, prior)
+{
+  
+  fvalmin <- Inf
+  
+  for (idx in 1:10) {
+    
+    initparam <- runif(nParam, 0, 1.0)
+    
+    res <- optim(initparam, func_minimize,
+                 hessian = TRUE, modelfunc = modelfunction, 
+                 data=data, prior = prior)
+    
+    if (res$value < fvalmin) {
+      paramest <- res$par
+      fvalmin <- res$value
+      lp <- -fvalmin
+      H <- res$hessian
+    }
+  }
+  negll <- fvalmin
+  paramlist <- paramest
+  
+  # ラプラス近似で対数周辺尤度を求める
+  lml <- lp + nParam/2 * log(2*pi) - 0.5 * log(det(H)) 
+  
+  print(sprintf("Estimated value: %.2f", paramest))
+  print(sprintf("log marginal likelihood: %.2f", lml))
+  
+  return(list(negll = negll, lml = lml, paramest = paramest))
+}
+
 
 # Q学習モデルのシミュレーションによるデータ生成 -------------------------------------------------
 
@@ -66,12 +199,12 @@ data <- list(reward=r,choice=c)
 
 # 最尤推定 --------------------------------------------------------------------
 cat("----------- ML -----------\n")
-resultML <- paramfitML(modelfunctions, data, nParamList)
+resultML <- paramfitML(func_qlearning, data, nParam)
 
 
 # MAP推定 -------------------------------------------------------------------
 cat("----------- MAP -----------\n")
-resultMAP <- paramfitMAP(modelfunctions, data, nParamList, priorList)
+resultMAP <- paramfitMAP(func_qlearning, data, nParam, priorList)
 
 
 # ベイズ推定 (MCMC) ------------------------------------------------------------
@@ -92,6 +225,7 @@ stanFit <- stan(file = 'qlearning_single_subject.stan',
 
 # 以下でRhatなどを表示し，サンプリングの精度を評価。
 # print(stanFit)
+
 
 # 結果の描画 -------------------------------------------------------------------
 

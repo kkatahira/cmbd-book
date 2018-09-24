1558c86a73577cd9e38e1c467df200bfae34a023
Kentaro Katahira <kkatahir@gmail.com>
2018-09-24 10:45:45 +0900

    update

--- chapter5/fqlearning_laplace_approx.R
+++ chapter5/fqlearning_laplace_approx.R
@@ -1,20 +1,26 @@
+#---------------------------------------------------------------------------#
+# 逆温度βを固定したF-Q学習モデルのラプラス近似
+#---------------------------------------------------------------------------#
+
 # clear 
 rm(list=ls())
 graphics.off()
 
+library(Rsolnp)
+
 source("model_functions_FQ.R")
-source("parameter_fit_functions_rsolnp.R")
+source("parameter_fit_functions.R")
 
 simulation_ID <- "FQlearning_siglesubject" # simulation ID for file name
 
-csv_simulation_data <- paste0("./simulation_data/simulation_data", simulation_ID, ".csv")
+csv_simulation_data <- paste0("./data/simulation_data", simulation_ID, ".csv")
 
 dt <- read.table(csv_simulation_data, header = T, sep = ",")
 data <- list(reward = dt$r, choice = dt$choice) 
 
-# plot likelihood (F-Q) ================================= # 
 
-# alphaL <- seq(0.01,0.99, by = 0.01)
+# plot exact posterior (likelihood)  ---------------------------------------------------
+
 alphaL <- seq(0.0,1, by = 0.01)
 
 llval <- numeric(length = length(alphaL))
@@ -23,7 +29,7 @@ for (idxa in 1:length(alphaL)) {
   llval[idxa] <- - func_minimize(c(alphaL[idxa], 2.0), 
                                       modelfunc = func_fqlearning, 
                                       data = data, 
-                                      prior = NULL #priorList[[3]]
+                                      prior = NULL 
   )
 }
 
@@ -40,12 +46,11 @@ plot(alphaL,exp(llval),
      cex.axis = 1.2,
      cex.main = 1.2)
 
-#----------------------------------------------------------#
-# Parameter fit MAP 
-#----------------------------------------------------------#
 
-# forgetting Q-learning (fixed beta)
-func_fqlearning <- function(param, data, prior = NULL)
+# Parameter fit MAP  ------------------------------------------------------
+
+# FQ-learning (fixed beta)
+func_fqlearning_fixed_beta <- function(param, data, prior = NULL)
 {
   
   alpha <- param[1]
@@ -55,7 +60,7 @@ func_fqlearning <- function(param, data, prior = NULL)
   r <- data$reward
   T <- length(c)
   
-  p1 <- numeric(T) 
+  pA <- numeric(T) 
   
   # set Q values (#option x T)
   Q <- matrix(numeric(2*T), nrow=2, ncol=T)
@@ -66,10 +71,10 @@ func_fqlearning <- function(param, data, prior = NULL)
   for (t in 1:T) {
     
     # choosing prob 1
-    p1[t] <- 1/(1+exp(-beta * (Q[1,t]-Q[2,t])))
-    p1[t] <- max(min(p1[t], 0.9999), 0.0001)
+    pA[t] <- 1/(1+exp(-beta * (Q[1,t]-Q[2,t])))
+    pA[t] <- max(min(pA[t], 0.9999), 0.0001)
     
-    ll <- ll + (c[t]==1) * log(p1[t]) +  (c[t]==2) * log(1-p1[t])
+    ll <- ll + (c[t]==1) * log(pA[t]) +  (c[t]==2) * log(1-pA[t])
     
     # update values 
     if (t < T) {
@@ -88,10 +93,10 @@ func_fqlearning <- function(param, data, prior = NULL)
     lprior <- dbeta(alpha,prior$alpha_a, prior$alpha_b,log = T) 
   }
   
-  return(list(negll = -ll - lprior,Q = Q, p1 = p1))
+  return(list(negll = -ll - lprior,Q = Q, pA = pA))
 }
 
-modelfunctions <- c(func_fqlearning)
+modelfunctions <- c(func_fqlearning_fixed_beta)
 
 nParamList <- c(1)
 
@@ -99,20 +104,12 @@ priorList <- list(
   list(alpha_a = 1, alpha_b = 1)
 )
 
-nModel <- length(nParamList)
 
-#-------solver----------------------------
-library(Rsolnp)
-nModel <- length(modelfunctions)
 lml <- numeric(nModel)
-negll <- numeric(nModel)
 paramlist <- list()
-hessian <- list()
-prior <- priorList
 
 idxm <- 1
 fvalmin <- Inf
-print(sprintf("Model %d:", idxm))
 
 for (idx in 1:10) {
   
@@ -120,29 +117,25 @@ for (idx in 1:10) {
   initparam <- runif(nParamList[idxm], 0, 1.0)
   
   res <- solnp(initparam, fun = func_minimize, 
-               # LB = lblist[[idxmodel]], UB = ublist[[idxmodel]], 
                modelfunc = modelfunctions[[idxm]], 
                control = list(trace = 0),
-               data = data, prior = prior[[idxm]])
+               data = data, prior = priorList[[idxm]])
   
   nll <- res$values[length(res$values)]
   
   if (nll < fvalmin) {
     paramest <- res$par
-    # fvalmin <- nll
     lp <- -nll
     H <- res$hessian
   }
 }
-negll[idxm] <- fvalmin
-paramlist[[idxm]] <- paramest
+paramlist <- paramest
 
 # log marginal likelihood (Laplace)
-#lml[idxm] <- lp + nParam[idxm]/2 * log(nParam[idxm]*pi) - 0.5 * log(det(H)) 
-lml[idxm] <- lp + nParamList[idxm]/2 * log(2*pi) - 0.5 * log(det(H)) 
+lml <- lp + nParamList/2 * log(2*pi) - 0.5 * log(det(H)) 
 
 print(sprintf("Estimated value: %.2f", paramest))
-print(sprintf("log marginal likelihood: %.2f", lml[idxm]))
+print(sprintf("log marginal likelihood: %.2f", lml))
 
 # laplace
 laplace <- function(lpMAP, mu, muMAP, H) {
@@ -157,11 +150,5 @@ lines(alphaL,exp(laplace(lp, alphaL, paramest[1], H[1])),
 legend("topright",
        lwd = c(2,2), 
        legend = c("exact", "Laplace"),
-       lty=c("solid","dashed")# , 
-       # bty = "n"
+       lty=c("solid","dashed")
 )
-
-# 
-# cat("----------- MAP -----------\n")
-# resultMAP <- paramfitMAP(modelfunctions, data, nParamList, priorList)
-
